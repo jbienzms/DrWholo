37fce6bbd9ee3931e6e7ca4b941e6fe081748562
Jared Bienz <jbienz@microsoft.com>
2016-07-23 00:55:31 -0500

    Serious updates to .gitignore. Built UWP project. Started with layout classes like Edge and Node.

---
+++ Unity/DrWholo/Assets/Scripts/Layout/Edge.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DrWholo.Layout
+{
+
+    public class Edge : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
